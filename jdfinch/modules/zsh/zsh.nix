db15ac8af9c7c8ec5bc39b0c5af5b514e17a72ea
James Finch <jdifnch@emory.edu>
2025-09-13 13:01:49 -0400

    zsh prompt

--- jdfinch/modules/zsh/zsh.nix
+++ jdfinch/modules/zsh/zsh.nix
@@ -7,6 +7,11 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
+    # This replaces the default prompt
+    promptInit = ''
+      PS1="%B%F{white}%K{blue}(%<...<%~)%<<%f%k > %b"
+    '';
+
     # Keep zsh config here to avoid conflicts with a .zshrc in jdfinch/home/
     initContent = ''
       alias renix='sudo nixos-rebuild switch --flake ~/nixos#raider'
