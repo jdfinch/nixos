6cad7071f7994685bda82e9e8239f214a25f645a
James Finch <jdifnch@emory.edu>
2025-09-13 14:31:37 -0400

    zsh startup and ls/cd

--- jdfinch/modules/zsh/startup.zsh
+++ jdfinch/modules/zsh/startup.zsh
@@ -1,15 +1,25 @@
-# Print PWD once when an interactive shell starts
-[[ -o interactive ]] && print -r -- "$PWD"
+# Only for interactive shells
+if [[ -o interactive ]]; then
+  print -r -- "$PWD"
 
-# After every directory change, run ls (use eza if available)
-autoload -Uz add-zsh-hook
-
-_ls_after_cd() {
-  if (( $+commands[eza] )); then
-    eza -la --group-directories-first
+  # tree -L 1 if available, else fallback to ls
+  if (( $+commands[tree] )); then
+    tree -L 1 --noreport
+  elif (( $+commands[eza] )); then
+    eza --group-directories-first
   else
-    ls -la
+    ls
+  fi
+  echo ""
+fi
+
+# Hook for after every cd
+autoload -Uz add-zsh-hook
+_chpwd_list() {
+  if (( $+commands[eza] )); then
+    eza --group-directories-first
+  else
+    ls
   fi
 }
-
-add-zsh-hook chpwd _ls_after_cd
+add-zsh-hook chpwd _chpwd_list
