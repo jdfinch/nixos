a35f7e413a5d9692724e02936314c4e6362e2ee9
James Finch <jdifnch@emory.edu>
2025-09-13 13:24:36 -0400

    zsh prompt

--- jdfinch/modules/zsh/prompt.zsh
+++ jdfinch/modules/zsh/prompt.zsh
@@ -1,16 +1,22 @@
-# Truncate cwd from the left if longer than given length
-_trunc_cwd() {
+# Always render cwd as exactly (maxlen-2) chars, plus " >"
+_fixed_cwd_prompt() {
   local maxlen=$1
   local p=${PWD/#$HOME/~}
-  if (( ${#p} > maxlen )); then
-    p="...${p: -$maxlen}"
+  local promptlen=$((maxlen - 2))   # leave space for " >"
+
+  if (( ${#p} > promptlen )); then
+    # truncate from left
+    p="...${p: -$((promptlen - 3))}"
+  else
+    # pad with spaces on right
+    p="${p}$(printf '%*s' $((promptlen - ${#p})))"
   fi
+
   print -r -- "$p"
 }
 
-# precmd is run before each prompt is displayed
 precmd() {
-  local maxlen=32   # change this if you want a different max
-  PROMPT="%B%F{white}$(_trunc_cwd $maxlen)%f > %b"
+  local maxlen=32
+  PROMPT="%B%F{white}$(_fixed_cwd_prompt $maxlen)%f > %b"
   RPROMPT=
 }
