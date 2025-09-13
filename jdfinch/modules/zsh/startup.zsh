773b1d30623f8f11982719e6aab8c7156cca4942
James Finch <jdifnch@emory.edu>
2025-09-13 14:26:31 -0400

    zsh prompt

--- jdfinch/modules/zsh/startup.zsh
+++ jdfinch/modules/zsh/startup.zsh
@@ -1,3 +1,15 @@
-chpwd() {
-    print -P "%F{235}${PWD}%f"
+# Print PWD once when an interactive shell starts
+[[ -o interactive ]] && print -r -- "$PWD"
+
+# After every directory change, run ls (use eza if available)
+autoload -Uz add-zsh-hook
+
+_ls_after_cd() {
+  if (( $+commands[eza] )); then
+    eza -la --group-directories-first
+  else
+    ls -la
+  fi
 }
+
+add-zsh-hook chpwd _ls_after_cd
