502d791e4b1384b8704aba3a295f4fc5e36c8920
James Finch <jdifnch@emory.edu>
2025-09-13 20:54:00 -0400

    wofi dark mode

--- jdfinch/modules/zsh/aliases.zsh
+++ jdfinch/modules/zsh/aliases.zsh
@@ -1,1 +1,11 @@
+
 alias renix='sudo nixos-rebuild switch --flake ~/nixos#raider'
+alias rehome="home-manager switch --flake ~/nixos#jdfinch"
+
+
+# Git quick commit: gcm "your message"
+git-commit() {
+  git add -A
+  git status
+  git commit -m "$1"
+}
